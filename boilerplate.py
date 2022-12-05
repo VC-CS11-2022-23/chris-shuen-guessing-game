dd27267733ff49b72012dfa19936502b107c4caf
CmoneyShuen <119447788+CmoneyShuen@users.noreply.github.com>
2022-12-05 22:03:08 +0000

    hehe

--- boilerplate.py
+++ boilerplate.py
@@ -13,3 +13,21 @@ while True:
     guess = int(input("Guess a number between 1 and 10: "))
 
     # implement your logic below
+
+    TRIES += 1
+
+    if guess == number:
+        print("CORRECT!")
+        break
+        
+    if TRIES == 3:
+        print("YOU LOSE! KILL YOURSELF!")
+        print("The correct number was:",number)
+        break
+
+    if guess > number:
+        print("too high")
+
+    else:
+        print("too low")
+   
